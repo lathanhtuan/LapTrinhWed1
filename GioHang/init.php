4080c2984093c5704b396d9058ec6cd1e0003f03
Tran Duy Quang <44082596+hacaoluoc@users.noreply.github.com>
2020-12-08 16:16:52 +0700

    Create init.php

---
+++ GioHang/init.php
@@ -0,0 +1,20 @@
+<?php
+//Khoi dong session
+session_start();
+
+//Composer autoload
+require_once 'vendor/autoload.php';
+
+//Cai dat hien thi error
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//Lien ket DB
+$db = new PDO('mysql:host=localhost;dbname=babyshop;charset=utf8', 'root', '');
+//lien ket voi file Function
+require_once 'functions.php';
+
+//Lay User hien tai
+$currentUser = getCurrentUser();
+?>
