e30e165806cefe52710a8fe1442b51f0935c7876
lathanhtuan <50731996+lathanhtuan@users.noreply.github.com>
2021-01-08 01:50:54 +0700

    Update CSDL(v1.1).sql

--- CSDL(v1.1).sql
+++ CSDL(v1.1).sql
@@ -113,18 +113,13 @@ CREATE TABLE `loaisanpham` (
 -- Dumping data for table `loaisanpham`
 --
 
-INSERT INTO `loaisanpham` (`MaLoaiSanPham`, `TenLoaiSanPham`, `BiXoa`) VALUES
-(1, 'Thú nhồi bông', 0),
-(2, 'Đồ chơi nhựa', 0),
-(3, 'Đồ chơi điện tử', 0),
-(4, 'Điều khiển từ xa', 0),
-(5, 'Đồ chơi trí tuệ', 0),
-(6, 'Đồ xịn', 0),
-(7, 'Đồ dỏm của CHINA', 0),
-(8, 'Đồ chời bưởi', 0),
-(9, 'Đồ chời bưởi', 0),
-(10, 'abc', 0);
-
+INSERT INTO `LoaiSanPham` (`MaLoaiSanPham`, `TenLoaiSanPham`, `BiXoa`) VALUES
+(1, 'Áo', 0),
+(2, 'Quần', 0),
+(3, 'Đồ chơi', 0),
+(4, 'Đồ Vest', 0),
+(5, 'Váy', 0),
+(6, 'Đồ bơi', 0);
 -- --------------------------------------------------------
 
 --
@@ -169,37 +164,52 @@ CREATE TABLE `sanpham` (
 -- Dumping data for table `sanpham`
 --
 
-INSERT INTO `sanpham` (`MaSanPham`, `TenSanPham`, `HinhURL`, `GiaSanPham`, `NgayNhap`, `SoLuongTon`, `SoLuongBan`, `SoLuocYeuThich`, `MoTa`, `BiXoa`, `MaLoaiSanPham`, `MaHangSanXuat`) VALUES
-(4, 'Bill D. Beaver', 'Lamaze_Bill_D_Beaver.jpeg', 160000, '2012-08-25 00:00:00', 14, 9, 25, 'Hải lý bằng bông mịn, đẹp, dễ thương', 0, 1, 3),
-(5, 'Captain Calamari', 'Lamaze_Captain_Calamari.jpeg', 180000, '2012-05-01 00:00:00', 14, 6, 4, 'Bạch tuộc biển', 0, 1, 3),
-(7, 'Elephantunes', 'Lamaze_Elephantunes.jpeg', 480000, '2012-09-12 00:00:00', 25, 2, 9, 'Voi bằng bông, hồng xì tin...', 0, 1, 3),
-(8, 'Freddie the Firefly', 'Lamaze_Freddie_Firefly.jpeg', 300000, '2012-07-03 00:00:00', 30, 0, 8, 'Bướm nhồi bông', 0, 1, 3),
-(9, 'Supper Mario', 'Lego_Supper_Mario.jpeg', 380000, '2012-01-01 00:00:00', 24, 6, 14, 'Bộ lấp áp Lego trò chơi Mario', 0, 5, 2),
-(10, 'Nasa Academy Space', 'Revell_Academy_Space.jpeg', 220000, '2012-08-15 00:00:00', 28, 7, 8, 'Tàu con thoi của Nasa', 0, 3, 1),
-(11, 'Lamborghini Revention', 'Revell_Lamborghini_Revention.jpeg', 260000, '2012-09-01 00:00:00', 38, 3, 40, 'Siêu xe Lamborghini Revention', 0, 3, 1),
-(12, 'Camaro SS', 'Revell_Camaro_SS.jpeg', 260000, '2012-10-02 00:00:00', 20, 0, 0, 'Xe đua Camaro SS', 0, 3, 1),
-(13, 'Pond Motion Gym', 'Lamaze_Pond_Motion_Gym.jpg', 920000, '2012-10-04 00:00:00', 10, 2, 14, 'Niệm lót cho trẻ em', 0, 1, 3),
-(14, 'Stacking Rings', 'Lamaze_Stacking_Rings.jpg', 240000, '2012-09-25 00:00:00', 5, 5, 1, 'Vòng xoay kỳ thú, kích thích sự tò mò của trẻ', 0, 1, 3),
-(15, 'Octivity Time', 'Lamaze_Octivity_Time.jpg', 270000, '2012-08-27 00:00:00', 19, 3, 3, 'Bé mặt trời xanh', 0, 1, 3),
-(16, 'Mittens the Kitten', 'Lamaze_Mittens_the_Kitten.jpg', 190000, '2012-07-13 00:00:00', 50, 3, 5, 'Chú mèo ngộ nghĩnh', 0, 1, 3),
-(17, 'Feel Me Fish', 'Lamaze_Feel_Me_Fish.jpg', 180000, '2012-09-15 00:00:00', 60, 3, 2, 'Chú cá vàng đa sắc', 0, 1, 3),
-(18, 'Huey the Hedgehog', 'Lamaze_Huey_the_Hedgehog.jpg', 200000, '2012-09-14 00:00:00', 30, 30, 22, 'Nhiếm bảy màu, mang đến sự may mắn cho bé', 0, 1, 3),
-(19, 'Neat-Oh!', 'Lego_Neat_Oh.jpg', 110000, '2012-06-12 00:00:00', 19, 13, 24, 'Túi đồ chơi xây dựng công viên của Lego', 0, 5, 2),
-(20, 'Ninjago 2172', 'Lego_Ninjago_2172.jpg', 160000, '2012-07-12 00:00:00', 20, 12, 13, 'Bộ xếp hình Lego thời Ai Cập cổ đại', 0, 5, 2),
-(21, 'Mexican', 'Lego_Mexican.jpg', 140000, '2012-08-17 00:00:00', 35, 12, 12, 'Bộ xếp hình Lego nhạc công Mehico', 0, 5, 2),
-(22, 'Star Wars', 'Lego_Star_Wars.jpg', 500000, '2012-10-05 00:00:00', 24, 28, 30, 'Bộ xếp hình Lego phi thuyền trong cuộc chiến tranh giữa các vì sao', 0, 5, 2),
-(23, 'City Park Cafe 3061', 'Lego_City_Park_Cafe_3061.jpg', 950000, '2012-10-07 00:00:00', 30, 2, 25, 'Bộ xếp hình xây dựng shop cafe trong thành phố ', 0, 5, 2),
-(24, 'Bright Lights Ball', 'Vtech_Bright_Lights_Ball.jpg', 150000, '2012-10-08 00:00:00', 39, 2, 6, 'Quả cầu thông minh', 0, 2, 4),
-(25, 'Baby\'s Laptop', 'Vtech_Baby_Laptop.jpg', 240000, '2012-09-07 00:00:00', 38, 2, 4, 'Laptop thông tin của trẻ em, giúp trẻ phát triển tư duy trí tuệ', 0, 2, 4),
-(26, 'Toot Driver Garage', 'Vtech_Toot_Driver_Garage.jpg', 620000, '2012-10-07 00:00:00', 20, 12, 30, 'Bãi đỗ xe trong thành phố, sẽ giúp bé tự điều hành việc hoạt động của một bãi đỗ xe hiện đại trong thành phố', 0, 2, 4),
-(27, 'Emergency Vehicles (3-Pack)', 'Vtech_Emergency_Vehicles.jpg', 223000, '2012-10-02 00:00:00', 20, 12, 3, 'Bộ sản phẩm 3 xe đồ chơi, đẹp, dễ thương và an toàn với trẻ', 0, 2, 4),
-(28, 'Lamborghini Murcielago', 'Rastar_Lamborghini_Murcielago.jpg', 300000, '2012-10-01 00:00:00', 10, 1, 3, 'Xe điều khiển từ xa Lamborghini', 0, 4, 5),
-(29, 'Rover Sport HSE', 'Rastar_Rover_Sport_HSE.jpg', 320000, '2012-09-30 00:00:00', 10, 3, 2, 'Xe điều khiển Rover, vượt mọi địa hình, sức mạnh của trâu', 0, 4, 5),
-(30, 'Apache Helicopter', 'Syma_Apache_Helicopter.jpg', 625000, '2012-10-01 00:00:00', 4, 2, 1, 'Máy bay chiến đâu siêu đa năng Apache', 0, 4, 6),
-(31, 'Micro Helicopter', 'Syma_Micro_Helicopter.jpg', 560000, '2012-10-05 00:00:00', 2, 6, 5, 'Máy bay lên thẳng đa dụng', 0, 4, 6),
-(32, 'Micro Chinook', 'Syma_ Micro_Chinook.jpg', 410000, '2012-10-06 00:00:00', 3, 0, 0, 'Máy bay trực thăng vận tải, đa dụng, có thể vận chuyển được xe tăng', 0, 4, 6),
-(33, 'X1 - 01', 'Syma_x1_01.jpg', 600000, '2012-10-06 00:00:00', 4, 0, 15, 'Máy bay lên thẳng 4 cánh quạt, đa dụng và dễ dàng điều khiển vượt qua mọi địa hình', 0, 4, 6);
-
+INSERT INTO `SanPham` (`MaSanPham`, `TenSanPham`, `HinhURL`, `GiaSanPham`, `NgayNhap`, `SoLuongTon`, `SoLuongBan`, `SoLuocYêuThích`, `MoTa`, `BiXoa`, `MaLoaiSanPham`, `MaHangSanXuat`) VALUES
+(4, 'Bill D. Beaver', '1.jpg', 160000, '2012-08-25 00:00:00', 14, 9, 25, 'Hải lý bằng bông mịn, đẹp, dễ thương', 0, 3, 3),
+(5, 'Captain Calamari', '2.jpg', 180000, '2012-05-01 00:00:00', 14, 6, 4, 'Bạch tuộc biển', 0, 3, 3),
+(7, 'Elephantunes', '3.jpg', 480000, '2012-09-12 00:00:00', 25, 2, 9, 'Voi bằng bông, hồng xì tin...', 0, 3, 3),
+(8, 'Freddie the Firefly', '4.jpg', 300000, '2012-07-03 00:00:00', 30, 0, 8, 'Bướm nhồi bông', 0, 3, 3),
+(9, 'Supper Mario', '5.jpg', 380000, '2012-01-01 00:00:00', 24, 6, 14, 'Bộ lấp áp Lego trò chơi Mario', 0, 3, 2),
+(10, 'Nasa Academy Space', '6.jpg', 220000, '2012-08-15 00:00:00', 28, 7, 8, 'Tàu con thoi của Nasa', 0, 3, 1),
+(11, 'Lamborghini Revention', '7.jpg', 260000, '2012-09-01 00:00:00', 38, 3, 38, 'Siêu xe Lamborghini Revention', 0, 3, 1),
+(12, 'Camaro SS', '8.jpg', 260000, '2012-10-02 00:00:00', 20, 0, 0, 'Xe đua Camaro SS', 0, 3, 1),
+(13, 'Pond Motion Gym', '9.jpg', 920000, '2012-10-04 00:00:00', 10, 2, 14, 'Niệm lót cho trẻ em', 0, 3, 3),
+(14, 'Stacking Rings', '10.jpg', 240000, '2012-09-25 00:00:00', 5, 5, 1, 'Vòng xoay kỳ thú, kích thích sự tò mò của trẻ', 0,3, 3),
+(15, 'Octivity Time', '11.jpg', 270000, '2012-08-27 00:00:00', 19, 3, 3, 'Bé mặt trời xanh', 0, 3, 3),
+(16, 'Mittens the Kitten', '12.jpg', 190000, '2012-07-13 00:00:00', 50, 3, 5, 'Chú mèo ngộ nghĩnh', 0, 3, 3),
+(17, 'Feel Me Fish', '13.jpg', 180000, '2012-09-15 00:00:00', 60, 3, 2, 'Chú cá vàng đa sắc', 0, 3, 3),
+(18, 'Huey the Hedgehog', '14.jpg', 200000, '2012-09-14 00:00:00', 30, 30, 22, 'Nhiếm bảy màu, mang đến sự may mắn cho bé', 0, 3, 3),
+(19, 'Neat-Oh!', '15.jpg', 110000, '2012-06-12 00:00:00', 19, 13, 24, 'Đồ Chơi Ghép Hình Duka Xây Dựng Thành Phố', 0, 3, 2),
+(20, 'Ninjago 2172', '16.jpg', 160000, '2012-07-12 00:00:00', 20, 12, 13, 'Bộ xếp hình Lego thời Ai Cập cổ đại', 0, 3, 2),
+(21, 'Mexican', '17.jpg', 140000, '2012-08-17 00:00:00', 35, 12, 12, 'Bộ xếp hình Lego nhạc công Mehico', 0, 3, 2),
+(22, 'Star Wars', '18.jpg', 500000, '2012-10-05 00:00:00', 24, 28, 30, 'Bộ xếp hình Lego phi thuyền trong cuộc chiến tranh giữa các vì sao', 0, 3, 2),
+(23, 'City Park Cafe 3061', '19.jpg', 950000, '2012-10-07 00:00:00', 30, 2, 24, 'Bộ xếp hình xây dựng shop cafe trong thành phố ', 0, 3, 2),
+(24, 'fishing and numerical ', '20.jpg', 150000, '2012-10-08 00:00:00', 39, 2, 6, 'Bộ đồ chơi câu cá và học số thông minh 63 chi tiết', 0, 3, 4),
+(25, 'Baby''s Laptop', '21.jpg', 240000, '2012-09-07 00:00:00', 38, 2, 4, 'Laptop thông tin của trẻ em, giúp trẻ phát triển tư duy trí tuệ', 0, 3, 4),
+(26, 'Toot Driver Garage', '22.jpg', 620000, '2012-10-07 00:00:00', 20, 12, 30, 'Bãi đỗ xe trong thành phố, sẽ giúp bé tự điều hành việc hoạt động của một bãi đỗ xe hiện đại trong thành phố', 0, 3, 4),
+(27, 'Emergency Vehicles (3-Pack)', '23.jpg', 223000, '2012-10-02 00:00:00', 20, 12, 3, 'Bộ sản phẩm 3 xe đồ chơi, đẹp, dễ thương và an toàn với trẻ', 0, 3, 4),
+(28, 'Lamborghini Murcielago', '24.jpg', 300000, '2012-10-01 00:00:00', 10, 1, 2, 'Xe điều khiển từ xa Lamborghini', 0, 3, 5),
+(29, 'Rover Sport HSE', '25.jpg', 320000, '2012-09-30 00:00:00', 10, 3, 2, 'Xe điều khiển Rover, vượt mọi địa hình, sức mạnh của trâu', 0, 3, 5),
+(30, 'Apache Helicopter', '26.jpg', 625000, '2012-10-01 00:00:00', 4, 2, 1, 'Máy bay chiến đâu siêu đa năng Apache', 0, 3, 1),
+(31, 'Micro Helicopter', '27.jpg', 560000, '2012-10-05 00:00:00', 2, 6, 5, 'Máy bay lên thẳng đa dụng điều khiển từ xa', 0, 3, 5),
+(32, 'Micro Chinook', '28.jpg', 410000, '2012-10-06 00:00:00', 3, 0, 0, 'Máy bay trực thăng vận tải, đa dụng, có thể vận chuyển được xe tăng', 0, 3, 1),
+(33, 'X1 - 01', '29.jpg', 600000, '2012-10-06 00:00:00', 4, 0, 15, 'Máy bay lên thẳng 4 cánh quạt, đa dụng và dễ dàng điều khiển vượt qua mọi địa hình', 0, 3, 1),
+(34, 'Áo thun nam Asos màu xanh','30.jpg ',450000,'2012-10-06 00:00:00',1, 0, 10,'Áo thun nam Asos màu xanh lá mạ, cổ chữ V kết hợp dáng ôm nhẹ nam tính, chất cotton cao cấp thoáng mát',0, 1, 1 ),
+(35, 'Váy Sue Wong ren đen','31.jpg',500000,'2012-10-06 00:00:00',2.1.15.'Thiết kế sang trọng, đẳng cấp đến từng chi tiết nhỏ nhất',0,5,3),
+(36, 'Váy voan FRENCH CONNECTION','32.jpg',650000,'2012-10-01 00:00:00',5,1,10,'Cổ tròn, không tay, dải eo ngang màu đen tạo điểm nhấn cho cả thiết kế thêm hoàn hảo',0,5,3),
+(37, 'Quần giữ nhiệt nam manhattan','33.jpg',530000, '2012-10-01 00:00:00',1,0,2,'Quần có thế mặc trong quần bò, quần âu,...giữ cho đôi chân của bạn luôn ấm áp và thoải mái',0,2,1),
+(38, 'Áo Giữ Nhiệt Kim Tuyến Cổ Cao','34.jpg',120000,'2012-10-01 00:00:00',1,2,10,'Độ co giãn, đàn hồi cao mang đến sự thoải mái khi vận động',0,1,3),
+(39, 'Đồ bơi một mảnh có tay khoá kéo','35.jpg',250000,'2012-10-01 00:00:00',3,1,6,' Áo có sẵn mút ngực, nên các nàng yên tâm mặc luôn mà không cần áo lót nhé',0,6,2),
+(40, 'Bikini, đồ bơi 1 mảnh liền xoắn ngực xanh cổ vịt sành điệu','36.jpg',450000,'2012-10-01 00:00:00',5,4,20,'Hàng Việt Nam chất lượng cao ',0,6,2),
+(41, 'Bikini 1 mảnh chéo cổ sành điệu','37.jpg',500000,'2012-10-01 00:00:00',3,1,19,'Hàng Việt Nam chất lượng cao ',0,6,2),
+(42, 'Aó sơ mi trắng kẻ sọc','38.jpg',260000,'2012-10-01 00:00:00',4,2,10,'Trắng của sự sang trọng, quý phái',0,1,1),
+(43, 'Aó sơ mi xanh ','39.jpg',310000,'2012-10-01 00:00:00',3,2,20,'Hàng Việt Nam chất lượng cao',0,1,2),
+(44, 'Váy trắng cổ thắt nơ HSU1','40.jpg',400000,'2012-10-01 00:00:00',4,2,10,'váy chất thô mềm tay dài cổ thắt nơ,nữ 40-53 kg cao 1m63 đổ về',0,5,2),
+(45, 'váy sơ mi trắng đi học, đi chơi','41.jpg',500000,'2012-10-01 00:00:00',5,2,16,'Chất vải mềm mịn, thấm hút mồ hôi, không bai, không xù, không dão',0,5,2),
+(46, 'Đồ chơi trẻ em lắp ráp Biệt Đội S.W.A.T Lele Brother 8522','42.jpg',600000,'2012-10-01 00:00:00',10,6,20,'là bộ đồ chơi giáo dục phát triển tư duy trẻ em mô phỏng đội đặc nhiệm swat hiện đại với các chú lính nhân vật lego cực dễ thương trên những phương tiện tuần tra',0,3,4),
+(47, 'Bộ lắp ráp Xe đua Công thức 1','43.jpg',500000, '2012-10-01 00:00:00',2,3,10,'được lấy ý tưởng từ các siêu xe trong những giải đua công thức 1',0,3,2),
+(48, 'Đồ chơi Lắp Ráp Robot Sandstorm Lele Brother - Transformer Fighter 3in1 8279','44.jpg',500000,'2012-10-01 00:00:00',2,1,5,'được chế tạo hoàn toàn từ chất liệu nhựa PET cao cấp, độ bền và chống biến dạng khi va đập tốt',0,3,2),
+(49, 'Bộ lắp ráp đội thi công công trình Construction Team 6in1 Lele Brother 8520','45.jpg',100000,'2012-10-01 00:00:00',1,0,2,'được lấy ý tưởng từ đội ngũ các chú kĩ sư xây dựng thành phố',0,3,1);
 -- --------------------------------------------------------
 
 --
